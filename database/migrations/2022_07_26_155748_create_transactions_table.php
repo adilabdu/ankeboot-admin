d771acf20a760e78eee1896f2da849edb4b9896b
adilabdu <adilabdu68@gmail.com>
2022-07-28 00:37:11 +0300

    Redefine structures for database tables

---
+++ database/migrations/2022_07_26_155748_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice');
+            $table->foreignId('book_id')
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->date('transaction_on');
+            $table->enum('type', ['sale', 'purchase']);
+            $table->integer('quantity');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
