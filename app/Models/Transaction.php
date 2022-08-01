3a1a9122d99ee203297073aeff881c1bff330fd8
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-08-01 17:59:58 +0300

    Cast transaction attribute type to TransactionType enum class

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -29,5 +30,6 @@ class Transaction extends Model
      */
     protected $casts = [
         'transaction_on' => 'date',
+        'type' => TransactionType::class,
     ];
 }
