36c4e8823edec1102832e28696160ee0a85e369c
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-08-04 18:17:11 +0300

    Try potential design for file input form

--- resources/js/pages/new/books.vue
+++ resources/js/pages/new/books.vue
@@ -46,23 +46,41 @@
         </Collapsable>
 
     </Form>
-    <Form
-        :show-submit="false"
-        submit-title="Upload"
-        subtitle="Upload an excel or comma separated value (CSV) file to register multiple book records at once. Make sure to include all the necessary columns and follow the template to insert the data successfully."
-        title="Insert multiple Book records from CSV"
-        :submit="createNewBook"
-    >
+<!--    <Form-->
+<!--        :show-submit="fileUploaded"-->
+<!--        submit-title="Upload"-->
+<!--        subtitle="Upload an excel or comma separated value (CSV) file to register multiple book records at once. Make sure to include all the necessary columns and follow the template to insert the data successfully."-->
+<!--        title="Insert multiple Book records from CSV"-->
+<!--        :submit="insertMultipleBooks"-->
+<!--    >-->
 
-        <FileInput />
+    <form @submit.prevent="insertMultipleBooks" title="Form" submit="" class="w-full grid grid-cols-12 gap-2">
+        <div class="col-span-4 py-2 flex flex-col gap-1">
+            <h1 class="text-base">Insert multiple Book records from CSV</h1>
+            <slot name="subtitle">
+                <h2 class="text-subtitle">
+                    Upload an excel or comma separated value (CSV) file
+                    to register multiple book records at once. Make sure to
+                    include all the necessary columns and follow the
+                    template to insert the data successfully.
+                </h2>
+            </slot>
+        </div>
+        <div class="flex flex-col col-span-8 items-end gap-6">
+            <FileInput class="w-full " v-model="uploadFile" />
+            <div class="w-full flex justify-end items-center">
+                <button type="submit" class="px-4 bg-brand-primary w-fit text-white rounded-md py-2">Upload</button>
+            </div>
+        </div>
+    </form>
 
-</Form>
+<!--</Form>-->
 
 </template>
 
 <script setup>
 
-    import { onMounted, ref } from "vue";
+    import { onMounted, ref, watch } from "vue";
     import ContentPage from "../../layouts/content-page.vue";
     import Form from "../../components/Form/Form.vue";
     import TextInput from "../../components/Form/TextInput.vue";
@@ -74,6 +92,13 @@
     import DatePicker from "../../components/Form/DatePicker.vue";
     import FileInput from "../../components/Form/FileInput.vue";
     import { useRoute } from "vue-router";
+
+    const fileUploaded = ref(false)
+    const uploadFile = ref(null)
+
+    watch(uploadFile, () => {
+        fileUploaded.value = !!uploadFile.value;
+    })
 
     const bookData = ref({
         title: '',
@@ -155,6 +180,26 @@
             .finally(() => {
             })
 
+    }
+
+    function insertMultipleBooks() {
+
+        store.dispatch('postMultipleBooks', {
+            file: uploadFile.value
+        }).then((response) => {
+
+            if (response === 'ok') {
+
+                router.push({ name: 'Books' })
+
+            } else {
+
+                alert(response)
+
+            }
+
+        })
+            .finally(() => {})
     }
 
     onMounted(() => {
