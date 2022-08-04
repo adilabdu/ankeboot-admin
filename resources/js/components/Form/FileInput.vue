e013b7459dc0309257db599f6343fe95a8714c3e
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-08-04 18:15:30 +0300

    Set up two-way binding for the value in FileInput

--- resources/js/components/Form/FileInput.vue
+++ resources/js/components/Form/FileInput.vue
@@ -17,16 +17,16 @@
     </svg>
 
     <div class="flex flex-col items-center justify-center gap-0.5 z-10">
-      <div :class="[file ? 'opacity-50' : '']" class="text-subtitle flex">
+      <div :class="[modelValue ? 'opacity-50' : '']" class="text-subtitle flex">
         <label>
           <span class="font-semibold text-brand-primary ">Upload a file</span>
           <input @change="getFileFromUpload" id="file-upload" name="file-upload" type="file" accept="text/csv" class="sr-only" />
         </label>
         <p class="font-medium">&nbsp;or drag and drop</p>
       </div>
-      <p v-if="!file" class="text-subtitle"> Only <span class="font-mono">CSV</span> file types are supported. </p>
+      <p v-if="!modelValue" class="text-subtitle"> Only <span class="font-mono">CSV</span> file types are supported. </p>
       <div v-else class="flex items-center gap-1.5">
-        <p class="text-subtitle font-medium"> <span class="font-mono text-brand-primary">{{ file.name }}</span> uploaded </p>
+        <p class="text-subtitle font-medium"> <span class="font-mono text-brand-primary">{{ modelValue.name }}</span> uploaded </p>
         <button @click="resetFile" class="z-10 bg-subtitle p-1 rounded-md cursor-pointer">
           <svg width="10" height="10" viewBox="0 0 12 12" fill="white" xmlns="http://www.w3.org/2000/svg">
             <path d="M10.192 0.343994L5.94903 4.58599L1.70703 0.343994L0.29303 1.75799L4.53503 5.99999L0.29303 10.242L1.70703 11.656L5.94903 7.41399L10.192 11.656L11.606 10.242L7.36403 5.99999L11.606 1.75799L10.192 0.343994Z" fill="white"/>
@@ -41,10 +41,17 @@
 
 <script setup>
 
-  import { ref } from "vue";
+  import { ref, computed } from "vue";
+
+  const props = defineProps({
+      modelValue: {
+          type: File,
+      }
+  })
+  const emit = defineEmits(['update:modelValue'])
 
   const dragRegionActivated = ref(false)
-  const file = ref(null)
+  const file = computed(() => props.modelValue)
 
   function toggleRegionActivation(event, activate) {
     dragRegionActivated.value = activate
@@ -55,7 +62,9 @@
 
     if (event.dataTransfer.items[0].getAsFile().type === 'text/csv') {
       dragRegionActivated.value = false
-      file.value = event.dataTransfer.items[0].getAsFile()
+        console.log('file successfully dropped ', event.dataTransfer.items[0].getAsFile())
+      emit('update:modelValue', event.dataTransfer.items[0].getAsFile())
+      // props.modelValue = event.dataTransfer.items[0].getAsFile()
       console.log('file is ', file.value)
 
     } else {
@@ -66,13 +75,15 @@
   }
 
   function getFileFromUpload(event) {
-    file.value = event.target.files[0]
+    // props.modelValue = event.target.files[0]
+    emit('update:modelValue', event.target.files[0])
     console.log('file is ', file.value)
   }
 
   function resetFile() {
     console.log('reset file')
-    file.value = null
+    emit('update:modelValue', null)
+    // props.modelValue = null
   }
 
 </script>
