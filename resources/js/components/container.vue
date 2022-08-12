70805f85bbfe4f4b30e346418bf4d0622060c173
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-08-12 18:16:33 +0300

    Create div with custom events

---
+++ resources/js/components/container.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div ref="container">
+        <slot />
+    </div>
+
+</template>
+
+<script setup>
+
+    import { ref } from "vue"
+
+    const emit = defineEmits(['resize'])
+
+    const container = ref(null)
+
+    addEventListener('resize', () => {
+
+        emit('resize', {
+            width: container.value.clientWidth,
+            height: container.value.clientHeight
+        })
+
+    })
+
+</script>
+
+<style scoped>
+
+</style>
