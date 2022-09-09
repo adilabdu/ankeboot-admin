3ae6899001d8ea2abe2465f91a25339fb7859aea
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-09-09 20:42:38 +0300

    Add pattern checking functionality with TextInput component

--- resources/js/components/Form/TextInput.vue
+++ resources/js/components/Form/TextInput.vue
@@ -5,12 +5,15 @@
             {{ label }}
             <span v-if="required" class="text-red-600">*</span>
         </label>
-        <input :disabled="disabled" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :required="required" type="text" :placeholder="placeholder" class="focus:outline-brand-primary focus:outline focus:outline-2 focus:outline-offset-2 h-10 w-full p-2 border border-border-light rounded-md" />
+        <input :disabled="disabled" :pattern="pattern" :value="modelValue" @input="validate($event.target.value)" :required="required" type="text" :placeholder="placeholder" class="peer focus:outline-brand-primary focus:outline focus:outline-2 focus:outline-offset-2 h-10 w-full p-2 border border-border-light rounded-md" />
+        <p v-if="error !== ''" class="peer-focus:inline hidden text-xs text-red-600">{{ error }}</p>
     </div>
 
 </template>
 
 <script setup>
+
+    import { ref, computed } from "vue"
 
     const props = defineProps({
         label: {
@@ -31,9 +34,54 @@
         disabled: {
             type: Boolean,
             default: false
+        },
+        pattern: {
+            type: String,
+            default: ''
         }
     })
-    defineEmits(['update:modelValue'])
+
+    const emits = defineEmits(['update:modelValue'])
+
+    const input = computed(() => props.modelValue)
+    const patternType = computed(() => {
+
+        switch (props.pattern) {
+
+            case '^[0-9]+.?[0-9]{0,2}$':
+                return 'price'
+
+            case '^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$':
+                return 'date'
+
+            default:
+                return 'text'
+
+        }
+
+    })
+    const error = ref('')
+
+    function validate(input) {
+
+        if (input === '') {
+
+            emits('update:modelValue', '')
+        
+        } else if (! new RegExp(props.pattern).test(input)) {
+
+            console.log(`Please input a valid ${patternType.value}`)
+            error.value = `Please input a valid ${patternType.value}`
+
+        } else {
+
+            error.value = ''
+            console.log('valid')
+            emits('update:modelValue', input)
+
+        }
+
+    }
 
 </script>
 
