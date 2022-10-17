0eb232f730492b35cf61a322a4363ca375f87712
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-10-17 16:21:43 +0300

    Add function to create new test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Deposit;
 use App\Models\DailySale;
@@ -13,6 +14,7 @@ use App\Enums\DepositType;
 use Exception;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,8 +32,26 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        $this->seedUser();
         $this->seedDailySales();
 
+    }
+
+    private function seedUser()
+    {
+        try {
+
+            User::create([
+                'name' => 'Test User',
+                'email' => 'testuser@test.com',
+                'username' => 'testuser',
+                'phone_number' => '0912345678',
+                'password' => Hash::make('helloworld')
+            ]);
+
+        } catch (Exception $e) {
+            dd('Error: ' . $e->getMessage());
+        }
     }
 
     private function seedDailySales()
@@ -42,7 +62,7 @@ class DatabaseSeeder extends Seeder
         // as such:
         // `is_submitted` = true
         // `cashier` = 'Elsa'
-        
+
 
         // Random data to generate
         // 1. # of sales receipts (1 or 2)
@@ -62,31 +82,31 @@ class DatabaseSeeder extends Seeder
                 $difference = rand(-5, 5);
                 $collected = 0.00;
                 $reported = 0.00;
-    
+
                 // update daily sale attribute
                 $dailySale->update([
                     'cashier' => 'Elsa'
                 ]);
-    
+
                 // generate random number of sales receipts
                 $salesReceipts = [];
                 for ($i = 0; $i < rand(1, 2); $i++) {
-    
+
                     $salesReceipts[] = [
                         'receipt' => 'Z:' . rand(10000, 99999),
                         'amount' => rand(1500, 20000) + (rand(0, 99) / 100),
                         'is_manual' => rand(0, 1) === 1,
                     ];
-    
+
                 }
                 $dailySale->sales_receipts()->createMany($salesReceipts);
-    
+
                 // get total amount of sales receipts
                 $salesReceiptTotalAmount = 0.00;
                 foreach ($salesReceipts as $sales) {
                     $salesReceiptTotalAmount += $sales['amount'];
                 }
-    
+
                 // generate a single deposit
                 $deposit = [
                     'amount' => rand(0, intval($salesReceiptTotalAmount)) + (rand(0, 99) / 100),
@@ -95,18 +115,18 @@ class DatabaseSeeder extends Seeder
                 ];
                 $dailySale->deposits()
                     ->save(new Deposit($deposit));
-    
+
                 // update total amount of sales receipts
                 $salesReceiptTotalAmount -= $deposit['amount'];
-    
+
                 // randomly generate an additional deposit
                 if (rand(0, 1000) % 5 === 0) {
                     $deposit = [
                         'amount' => rand(0, intval($salesReceiptTotalAmount)) + (rand(0, 99) / 100),
                         'type' => [
-                                DepositType::CASH, 
-                                DepositType::CARD, 
-                                DepositType::CHEQUE, 
+                                DepositType::CASH,
+                                DepositType::CARD,
+                                DepositType::CHEQUE,
                                 DepositType::WITHHOLDING,
                                 DepositType::TRANSFER,
                             ][rand(0, 4)],
@@ -114,48 +134,48 @@ class DatabaseSeeder extends Seeder
                     ];
                     $dailySale->deposits()
                         ->save(new Deposit($deposit));
-                
+
                     // update total amount of sales receipts
                     $salesReceiptTotalAmount -= $deposit['amount'];
-                
+
                 }
-    
+
                 // generate random number of credit sales
                 if ($salesReceiptTotalAmount >= 500 && (rand(0, 1000) % 7) === 0) {
-    
+
                     $deposit = [
                         'amount' => rand(500, intval($salesReceiptTotalAmount)) + (rand(0, 99) / 100),
                         'type' => DepositType::CREDIT,
                         'deposited_on' => $dailySale['date_of'],
                     ];
-    
+
                     $dailySale->deposits()
                         ->save(new Deposit($deposit))
                         ->credit()->save(new Credit([
                             'receipt' => 'FS' . rand(10000, 99999),
                             'client_name' => fake()->company(),
                         ]));
-    
+
                     // update total amount of sales receipts
                     $salesReceiptTotalAmount -= $deposit['amount'];
-    
+
                 }
-    
+
                 // generate random number of expenses
                 if (rand(0, 1000) % 4 === 0) {
-                        
+
                         $expense = [
                             'receipt' => 'PV' . rand(10000, 99999),
                             'amount' => rand(0, intval($salesReceiptTotalAmount)) + (rand(0, 99) / 100),
                             'description' => 'Consignment payment #' . rand(1, 4) . ' for ' . fake()->name(),
                         ];
-        
+
                         $dailySale->expenses()
                             ->save(new Expense($expense));
-        
+
                         // update total amount of sales receipts
                         $salesReceiptTotalAmount -= $expense['amount'];
-        
+
                 }
 
                 // generate an additional deposit to cover the difference
@@ -166,20 +186,20 @@ class DatabaseSeeder extends Seeder
                 ];
                 $dailySale->deposits()
                     ->save(new Deposit($deposit));
-                
+
                 // update total amount of sales receipts
                 $salesReceiptTotalAmount -= $deposit['amount'];
-    
+
                 $dailySale->is_submitted = true;
                 $dailySale->difference = round($salesReceiptTotalAmount, 2) * -1;
                 $dailySale->save();
-    
+
             }
 
             DB::commit();
 
         } catch (Exception $e) {
-            
+
             DB::rollBack();
             dd('Error: ' . $e->getMessage());
 
