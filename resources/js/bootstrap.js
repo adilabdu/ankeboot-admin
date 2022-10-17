75f4d29f8ecc0ba9253d26087b81f81ecfe383f3
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-10-17 15:51:00 +0300

    Setup Laravel Echo for listening to broadcasts from backend

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 window._ = _;
 
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -20,17 +23,16 @@ window.axios.defaults.headers.common['database'] = localStorage.getItem('connect
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+window.Pusher = Pusher;
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    // wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    // wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    // wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    // enabledTransports: ['ws', 'wss'],
+});
