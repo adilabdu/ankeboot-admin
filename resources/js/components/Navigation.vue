6e88d7beeabbb5a22f24199df76c31f6f43009d0
adilabdu <adilabdu68@gmail.com>
2022-11-20 11:57:24 +0300

    Close Navigation on click outside in mobile view

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,22 +1,22 @@
 <template>
 
   <!--  TODO: remove min-w-[16rem] or not?-->
-  <nav :class="[navigationOpened ? 'xl:-translate-x-0' : 'xl:-translate-x-full']" class="z-20 h-screen min-w-[16rem] bg-white border-r border-border-light xl:absolute xl:top-0 xl:left-0 flex flex-col transition-transform duration-300">
+  <nav ref="navigation" :class="[navigationOpened ? 'xl:-translate-x-0' : 'xl:-translate-x-full']" class="z-20 h-screen min-w-[16rem] bg-white border-r border-border-light xl:absolute xl:top-0 xl:left-0 flex flex-col transition-transform duration-300">
 
-      <div class="w-full h-[4rem] px-4 flex items-center justify-between">
+      <div class="w-full min-h-[4rem] px-4 flex items-center justify-between">
 
           <div class="leading-none text-left">
               <h1 class="font-semibold text-base text-brand-primary">Ankeboot</h1>
               <h1 class="text-xs text-subtitle">Management System</h1>
           </div>
 
-          <svg @click="toggleNavigation" class="hidden xl:inline" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2.70902 0.505398L0.163434 3.05098L5.2546 8.14215L0.163434 13.2333L2.70902 15.7789L7.80019 10.6877L12.8914 15.7789L15.4369 13.2333L10.3458 8.14215L15.4369 3.05098L12.8914 0.505398L7.80019 5.59657L2.70902 0.505398Z" fill="#6A727F"/>
+          <svg @click="toggleNavigation" class="w-4 h-4  stroke-[2.5] stroke-subtitle" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
 
       </div>
 
-      <ul class="flex flex-col grow py-2 px-2">
+      <ul class="flex flex-col grow py-2 px-2 overflow-y-auto">
 
           <slot />
 
@@ -29,7 +29,17 @@
 <script setup>
 
     import store from "../store"
-    import { computed } from "vue"
+    import { onClickOutside } from "@vueuse/core";
+    import { computed, ref } from "vue"
+
+    const navigation = ref(null)
+    onClickOutside(navigation, () => {
+
+        if (navigationOpened.value) {
+            store.dispatch('toggleNavigation', false)
+        }
+
+    })
 
     const navigationOpened = computed(() => store.state.ui.navigationOpened)
 
