08cd766f6639187e1bc3dcb1b1f5e3e2908236f3
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-11-28 09:18:20 +0300

    Optimize SwitchInput curve and button

--- resources/js/components/Form/SwitchInput.vue
+++ resources/js/components/Form/SwitchInput.vue
@@ -11,8 +11,8 @@
         ]" class="flex">
         <label class="text-subtitle font-medium whitespace-nowrap">{{ label }}</label>
         <div :class="[labelLocation === 'top' ? 'h-10 w-full flex justify-start items-center' : '']">
-            <div tabindex="0" @keydown.enter="$emit('update:modelValue', toggleRadio())" @click="$emit('update:modelValue', toggleRadio())" :class="[radioState ? 'bg-brand-primary border-brand-primary' : 'bg-wallpaper border-border-dark']" class="transition-colors duration-500 focus:outline-brand-primary sm:focus:outline-none focus:outline focus:outline-2 focus:outline-offset-2 cursor-pointer h-6 w-12 flex rounded-full relative border items-center">
-                <div :class="[radioState ? 'translate-x-full border-white' : '-translate-x-0.5 border-border-dark']" class="bg-white m-1 transition-all duration-500 w-5 h-5 border rounded-[100%]"></div>
+            <div tabindex="0" @keydown.enter="$emit('update:modelValue', toggleRadio())" @click="$emit('update:modelValue', toggleRadio())" :class="[radioState ? 'bg-brand-primary border-brand-primary' : 'bg-wallpaper border-border-dark']" class="transition-colors duration-500 focus:outline-brand-primary sm:focus:outline-none focus:outline focus:outline-2 focus:outline-offset-2 cursor-pointer h-[1.7rem] w-12 flex rounded-full relative border items-center">
+                <div :class="[radioState ? 'translate-x-[1.5rem] border-white' : 'translate-x-[0.2rem] border-border-dark']" class="bg-white transition-all duration-500 w-[1.15rem] h-[1.15rem] border rounded-[100%]"></div>
             </div>
         </div>
     </div>
