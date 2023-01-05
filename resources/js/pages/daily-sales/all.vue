c9330b098bd6d12d08acec10c1b56e265e22b8c9
adilabdu <adilabdu68@gmail.com>
2023-01-05 13:41:22 +0300

    Toggle daily sales statistics by time with new buttons

--- resources/js/pages/daily-sales/all.vue
+++ resources/js/pages/daily-sales/all.vue
@@ -1,12 +1,20 @@
 <template>
 
     <div class="w-full h-fit flex flex-col gap-2 items-end">
-<!--        <DropDown-->
-<!--            v-model="range"-->
-<!--            class="text-sm font-normal min-w-[133px] w-fit h-8"-->
-<!--            :list="['Last week', 'Last month', 'All time']"-->
-<!--        ></DropDown>-->
-        <div class="w-full grid grid-cols-4 gap-2 sm:grid-cols-1 sm:grid-rows-1">
+        <div class="w-full grid grid-cols-4 gap-2 sm:grid-cols-1 sm:grid-rows-1 relative">
+
+            <div class="top-0 left-0 h-full py-1 -translate-x-[2.5rem] absolute flex flex-col justify-between">
+
+                <button @click="decreaseRange" :disabled="range === 0" class="scale-95 focus:outline-none transition-shadow duration-150 disabled:hover:shadow-sm hover:shadow-md disabled:opacity-50 p-2 rounded-lg border border-border-dark bg-white shadow-sm">
+                    <ChevronUpIcon class="scale-110 stroke-subtitle w-4 h-4" />
+                </button>
+
+                <button @click="increaseRange" :disabled="range === 2" class="scale-95 focus:outline-none transition-shadow duration-150 disabled:hover:shadow-sm hover:shadow-md disabled:opacity-50 p-2 rounded-lg border border-border-dark bg-white shadow-sm">
+                    <ChevronDownIcon class="scale-110 stroke-subtitle w-4 h-4" />
+                </button>
+
+            </div>
+
             <InfoCard class="group col-span-1  sm:row-span-1" label="Unsubmitted Daily Sales">
                 <div v-if="! loading" class="w-full flex items-center justify-between">
                     <div class="flex items-center gap-2">
@@ -22,14 +30,24 @@
                     <path d="M17.4706 5.94533C18.1987 5.55015 18.4769 4.63079 17.9774 3.96992C17.0157 2.69771 15.7589 1.66786 14.3065 0.974827C12.4006 0.0653761 10.2557 -0.217352 8.1792 0.167163C6.10271 0.551679 4.20125 1.5837 2.74734 3.11531C1.29344 4.64692 0.361732 6.59949 0.0857576 8.69317C-0.190217 10.7869 0.203708 12.9142 1.21108 14.7702C2.21844 16.6262 3.78753 18.1157 5.69346 19.0252C7.59939 19.9346 9.74431 20.2174 11.8208 19.8328C13.4032 19.5398 14.8839 18.8708 16.1424 17.8912C16.7961 17.3824 16.7656 16.4223 16.1648 15.852V15.852C15.5639 15.2816 14.621 15.3229 13.9359 15.7887C13.1386 16.3308 12.2332 16.7055 11.2746 16.883C9.82102 17.1521 8.31958 16.9542 6.98542 16.3176C5.65127 15.681 4.55291 14.6384 3.84775 13.3391C3.1426 12.0399 2.86685 10.5508 3.06003 9.08522C3.25321 7.61964 3.90541 6.25284 4.92314 5.18072C5.94087 4.10859 7.27189 3.38618 8.72544 3.11701C10.179 2.84785 11.6804 3.04576 13.0146 3.68238C13.8945 4.10224 14.6718 4.69872 15.3015 5.42894C15.8424 6.05634 16.7425 6.34051 17.4706 5.94533V5.94533Z" fill="#FF8100"/>
                 </svg>
             </InfoCard>
-            <InfoCard class="col-span-1 sm:row-span-1" label="Deposit / Report difference">
+            <InfoCard class="col-span-1 sm:row-span-1">
+
+                <template #label>
+                    <p class="flex gap-2 items-center">
+                        <span>Deposit / Report difference</span>
+                        <span class="text-subtitle/75">
+                            {{ rangeOptions[range] }}
+                        </span>
+                    </p>
+                </template>
+
                 <div class="flex justify-between items-center gap-2 w-full h-full">
                     <div v-if="! loading" class="items-center gap-1 flex">
                         <span class="text-base">({{ statistics['aggregate'].difference.all > 0 ? '+' : '-' }})</span>
-                        <template v-if="range === 'Last week'">
+                        <template v-if="range === 2">
                             <span class="">{{ formatPrice(Math.abs(statistics['aggregate']['difference']['last_week'])) }}</span>
                         </template>
-                        <template v-else-if="range === 'Last month'">
+                        <template v-else-if="range === 1">
                             <span class="">{{ formatPrice(Math.abs(statistics['aggregate']['difference']['last_month'])) }}</span>
                         </template>
                         <template v-else>
@@ -42,13 +60,23 @@
                     </svg>
                 </div>
             </InfoCard>
-            <InfoCard class="col-span-1 sm:row-span-1" label="Sales Aggregate">
+            <InfoCard class="col-span-1 sm:row-span-1">
+
+                <template #label>
+                    <p class="flex gap-2 items-center">
+                        <span>Sales Aggregate</span>
+                        <span class="text-subtitle/75">
+                            {{ rangeOptions[range] }}
+                        </span>
+                    </p>
+                </template>
+
                 <div class="flex justify-between items-center gap-2 w-full h-full">
                     <div v-if="! loading" class="items-center gap-1 flex">
-                        <template v-if="range === 'Last week'">
+                        <template v-if="range === 2">
                             <span class="">{{ (formatPrice(statistics['aggregate'].sales['last_week'])) }}</span>
                         </template>
-                        <template v-else-if="range === 'Last month'">
+                        <template v-else-if="range === 1">
                             <span class="">{{ (formatPrice(statistics['aggregate'].sales['last_month'])) }}</span>
                         </template>
                         <template v-else>
@@ -61,13 +89,24 @@
                     </svg>
                 </div>
             </InfoCard>
-            <InfoCard class="col-span-1 sm:row-span-1" label="Unsettled Credit Sales">
+            <InfoCard class="col-span-1 sm:row-span-1">
+
+                <template #label>
+                    <p class="flex gap-2 items-center">
+                        <span>Unsettled Credit Sales</span>
+                        <span class="text-subtitle/75">
+                            {{ rangeOptions[range] }}
+                        </span>
+                    </p>
+                </template>
+
+
                 <div class="flex justify-between items-center gap-2 w-full h-full">
                     <div v-if="! loading" class="items-center gap-1 flex">
-                        <template v-if="range === 'Last week'">
+                        <template v-if="range === 2">
                             {{ (formatPrice(statistics['aggregate'].credits['last_week'])) }}
                         </template>
-                        <template v-else-if="range === 'Last month'">
+                        <template v-else-if="range === 1">
                             {{ (formatPrice(statistics['aggregate'].credits['last_month'])) }}
                         </template>
                         <template v-else>
@@ -93,13 +132,23 @@
     import DropDown from "../../components/Dropdown.vue"
     import BarChart from "../../components/Graphs/BarChart.vue";
     import LineChart from "../../components/Graphs/LineChart.vue";
+    import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/vue/20/solid"
     import axios from "axios";
     import store from "../../store";
 
     const loading = ref(true)
     const statistics = ref({})
-    const range = ref('All time')
+    const range = ref(0)
+    const rangeOptions = ref(['All time', 'Last Month', 'Last Week'])
     const controller = new AbortController()
+
+    function increaseRange() {
+        range.value++;
+    }
+
+    function decreaseRange() {
+        range.value--;
+    }
 
     async function getStatistics() {
 
