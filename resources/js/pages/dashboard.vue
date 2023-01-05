e25fa933f3182453adef3a158eb78e30e01a7f92
adilabdu <adilabdu68@gmail.com>
2023-01-05 13:39:34 +0300

    Fetch statistics data and feed into Graphs in dashboard page

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -3,14 +3,33 @@
     <ContentPage>
 
         <div class="grid grid-cols-3 gap-2 sm:grid-rows-3 sm:grid-cols-1">
-            <BarChart :data="monthlySales" :loading="!! ! monthlySales" />
-            <LineChart
-                background-color='#FF810020'
-                :data="[32, 40, 32, 44, 58, 42, 70, 70, 50, 43, 33, 29]"
-                title="Monthly subscribers"
-                subtitle="Mailing list registrations for each month of the year"
+            <BarChart
+                :data="monthlyPurchases"
+                :loading="!! ! monthlyPurchases"
+                title="Monthly Purchases"
+                subtitle="Purchases made within the past 12 months"
             />
-            <BarChart :data="monthlyPurchases" :loading="!! ! monthlyPurchases" />
+            <DonutChart
+                :loading="!! ! yearlyConsignmentSales"
+                :ratio="yearlyConsignmentSales"
+                subtitle="Ratio of consignment to cash sales within the last year"
+                title="Rate of Consignment Sales"
+                :legends="{
+                    first: `Percentage of cash sales made within the past year (${getLastYear()} - ${new Date().toLocaleDateString()})`,
+                    second: `Percentage of consignment sales made within the past year (${getLastYear()} - ${new Date().toLocaleDateString()})`
+                }"
+            />
+            <LineChart :data="monthlySales" :loading="!! ! monthlySales" />
+            <PieChart
+                :loading="!! ! yearlyConsignmentSales"
+                :ratio="yearlyConsignmentSales"
+                subtitle="Ratio of consignment to cash sales within the last year"
+                title="Rate of Consignment Sales"
+                :legends="{
+                    first: `Percentage of cash sales made within the past year (${getLastYear()} - ${new Date().toLocaleDateString()})`,
+                    second: `Percentage of consignment sales made within the past year (${getLastYear()} - ${new Date().toLocaleDateString()})`
+                }"
+            />
         </div>
 
         <ActionMenu />
@@ -25,6 +44,8 @@
     import ContentPage from "../layouts/content-page.vue";
     import InfoCard from "../components/InfoCard.vue";
     import BarChart from "../components/Graphs/BarChart.vue";
+    import DonutChart from "../components/Graphs/DonutChart.vue";
+    import PieChart from "../components/Graphs/PieChart.vue";
     import LineChart from "../components/Graphs/LineChart.vue";
     import { formatNumber } from "../utils";
     import ActionMenu from "../components/ActionMenu.vue";
@@ -36,7 +57,6 @@
             .then(response => {
 
                 monthlySales.value = response.data.data
-                console.log(monthlySales.value)
 
             })
             .catch(error => {
@@ -64,10 +84,37 @@
 
     }
 
+    const yearlyConsignmentSales = ref(0)
+    function getYearlyConsignmentSales() {
+
+        axios.get('/api/statistics/sales/consignments')
+            .then(response => {
+
+                yearlyConsignmentSales.value = response.data.data
+
+            })
+            .catch(error => {
+
+                alert(`Error while fetching yearly consignment sales: ${error}`)
+
+            })
+
+    }
+
+    function getLastYear() {
+
+        const date = new Date()
+        date.setFullYear(date.getFullYear() - 1)
+
+        return date.toLocaleDateString()
+
+    }
+
     onMounted(() => {
 
         getSales()
         getPurchases()
+        getYearlyConsignmentSales()
 
     })
 
