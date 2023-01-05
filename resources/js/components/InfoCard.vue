46347fc3190579acbdd3a7432c304ac74d34b60e
adilabdu <adilabdu68@gmail.com>
2023-01-05 13:39:08 +0300

    Add option to set label by a named #label slot

--- resources/js/components/InfoCard.vue
+++ resources/js/components/InfoCard.vue
@@ -2,7 +2,11 @@
 
   <div class="min-w-[275px] flex flex-col gap-0.5 justify-start p-6 sm:w-full h-28 rounded-2xl bg-white border-[0.5px] border-border-light drop-shadow-sm">
 
-    <label class="font-semibold text-subtitle uppercase text-xs">{{ label }}</label>
+    <label class="font-semibold text-subtitle uppercase text-xs">
+        <slot name="label">
+            {{ label }}
+        </slot>
+    </label>
     <div class="flex gap-2 items-center justify-start">
         <div class="text-3xl font-medium flex gap-2 items-center w-full">
           <slot v-if="! contentLoading">
@@ -26,7 +30,6 @@
   const props = defineProps({
     label: {
       type: String,
-      required: true,
     },
     content: {
       type: String,
