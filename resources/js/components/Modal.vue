12e9296d23779cb4e9ae83d0739e9298e081f97a
adilabdu <adilabdu68@gmail.com>
2023-01-24 10:05:01 +0300

    Remove scoped custom scrollbar styles

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="modal w-full h-screen overflow-y-auto py-12 sm:py-0 sm:overflow-hidden fixed bg-black/80 flex items-start sm:items-end justify-center z-[100]">
+    <div class="scrollbar-brand w-full h-screen overflow-y-auto py-12 sm:py-0 sm:overflow-hidden fixed bg-black/80 flex items-start sm:items-end justify-center z-[100]">
 
         <slot />
 
@@ -23,28 +23,5 @@
 </script>
 
 <style scoped>
-
-    .modal::-webkit-scrollbar {
-        width: 16px;
-    }
-    .modal::-webkit-scrollbar-corner {
-        background: rgba(0,0,0,0);
-    }
-    .modal::-webkit-scrollbar-thumb {
-        background-color: rgba(116,116,116,1);
-        border-radius: 6px;
-        border: 4px solid rgba(0,0,0,0);
-        background-clip: content-box;
-        transition: all;
-        transition-duration: 1s;
-    }
-
-    .modal::-webkit-scrollbar-track {
-        background-color: rgba(0,0,0,0);
-    }
-
-    .modal:hover .modal::-webkit-scrollbar-thumb {
-        background-color: rgba(116,116,116,1) !important;
-    }
 
 </style>
