cf765a0a51f212821011fdaff37656d87de59cd6
adilabdu <adilabdu68@gmail.com>
2023-01-24 10:05:46 +0300

    Implement minimized navigation option

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,21 +1,26 @@
 <template>
 
-  <nav ref="navigation" :class="[navigationOpened ? 'xl:-translate-x-0' : 'xl:-translate-x-full']" class="z-20 h-screen min-w-[16rem] bg-white sm:border-t border-r border-border-light xl:absolute xl:top-0 xl:left-0 flex flex-col transition-transform duration-300">
+  <nav ref="navigation" :class="[navigationOpened ? 'xl:-translate-x-0' : 'xl:-translate-x-full', mini ? 'w-16' : 'min-w-[16rem]']" class="z-20 h-screen bg-white sm:border-t border-r border-border-light xl:absolute xl:top-0 xl:left-0 flex flex-col transition-transform duration-300">
 
-      <div class="w-full min-h-[4rem] px-4 flex items-center justify-between">
+      <div class="w-full min-h-[4rem] flex items-center" :class="[mini ? 'justify-center' : 'px-4 justify-between']">
 
-          <div class="leading-none text-left">
+          <div @click="minimizeNavigation" :class="{ 'hidden': mini }" class="leading-none text-left">
               <h1 class="font-semibold text-base text-brand-primary">Ankeboot</h1>
               <h1 class="text-xs text-subtitle">Management System</h1>
           </div>
 
-          <svg @click="toggleNavigation" class="hidden xl:inline w-4 h-4 stroke-[2.5] stroke-subtitle" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <div @click="minimizeNavigation" :class="{ 'hidden': !mini }" class="group flex translate-x-1.5">
+              <div class="w-6 h-6 rounded-[0.2rem] bg-brand-primary/50 rotate-45" />
+              <div class="w-6 h-6 rounded-[0.2rem] bg-[#E2F7BB]/75 rotate-45 -translate-x-3" />
+          </div>
+
+          <svg @click="toggleNavigation" :class="[ mini ? '' : 'xl:inline' ]" class="hidden w-4 h-4 stroke-[2.5] stroke-subtitle" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
 
       </div>
 
-      <ul class="flex flex-col grow py-2 px-2 overflow-y-auto">
+      <ul class="flex flex-col grow py-2 px-2 overflow-y-auto scrollbar-brand">
 
           <slot />
 
@@ -30,6 +35,16 @@
     import store from "../store"
     import { onClickOutside } from "@vueuse/core";
     import { computed, ref } from "vue"
+    import logo from "../../assets/favicon/android-chrome-512x512.png"
+
+    const props = defineProps({
+        minimize: {
+            type: Boolean,
+            default: true
+        }
+    })
+
+    const mini = computed(() => props.minimize)
 
     const navigation = ref(null)
     onClickOutside(navigation, () => {
@@ -44,6 +59,10 @@
 
     function toggleNavigation() {
         store.dispatch('toggleNavigation')
+    }
+
+    function minimizeNavigation() {
+        store.dispatch('toggleNavigationMinimize')
     }
 
 </script>
