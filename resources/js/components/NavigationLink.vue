262862feba8b81258a7eb05d857f5f1f673da406
adilabdu <adilabdu68@gmail.com>
2023-01-24 10:05:52 +0300

    Implement minimized navigation option

--- resources/js/components/NavigationLink.vue
+++ resources/js/components/NavigationLink.vue
@@ -5,24 +5,20 @@
         v-slot="{ isActive }"
         @click="navigate"
         active-class="!hover:bg-brand-primary !bg-brand-primary"
-        class="relative bg-white hover:bg-brand-secondary focus:outline-none cursor-pointer grid grid-cols-12 px-2 py-3 rounded-md capitalize flex items-center gap-4"
+        :class="[ mini ? 'w-full aspect-square' : 'grid grid-cols-12 px-2 py-3' ]"
+        class="relative bg-white hover:bg-brand-secondary focus:outline-none cursor-pointer rounded-md capitalize flex items-center gap-4"
     >
-
-        <div class="col-span-2">
+        <div :class="[ mini ? 'w-full h-full grid place-items-center' : 'col-span-2']">
             <slot :active="isActive" />
         </div>
 
-        <div class="col-span-8">
+        <div :class="[ mini ? 'hidden' : 'col-span-8']">
             <label :class="[isActive ? 'text-white font-medium' : 'text-black']" class="">{{ label }}</label>
         </div>
 
-        <div :class="[isActive ? 'bg-white' : 'bg-brand-secondary']" v-if="number" class="absolute w-7 h-7 rounded-full right-2 flex items-center justify-center">
+        <div :class="[isActive ? 'bg-white' : 'bg-brand-secondary', mini ? 'hidden' : '']" v-if="number" class="absolute w-7 h-7 rounded-full right-2 flex items-center justify-center">
             <p class="text-xs text-brand-primary font-medium">{{ number }}</p>
         </div>
-
-<!--        <div class="flex items-center justify-center rounded-full w-3/4 aspect-square bg-white col-span-2">-->
-<!--            <p class="text-xs text-brand-primary">3</p>-->
-<!--        </div>-->
 
     </router-link>
 
@@ -46,10 +42,15 @@
         },
         number: {
             type: [Number, String]
+        },
+        minimize: {
+            type: Boolean,
+            default: true
         }
 
     })
 
+    const mini = computed(() => props.minimize)
     const active = computed(() => router)
 
     function navigate() {
