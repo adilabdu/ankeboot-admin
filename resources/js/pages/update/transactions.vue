a32d550288d1620dac17112c8093915e1e4d6386
adilabdu <adilabdu68@gmail.com>
2023-02-18 12:19:44 +0300

    Set up `update/transactions` page for new implementation

--- resources/js/pages/update/transactions.vue
+++ resources/js/pages/update/transactions.vue
@@ -7,16 +7,48 @@
             Do not forget to specify the transaction type (purchase / sale).
         </template>
 
-        <TextInput disabled v-model="transactionData.book_title" class="w-full" placeholder="Book title for Transaction" label="Book for Transaction" />
+        <div class="flex gap-8 w-full">
+            <TextInput disabled v-model="transactionData.book_title" class="w-full" placeholder="Book title for Transaction" label="Book for Transaction" />
+            <SwitchInput class="px-3" label-location="top" v-model="transactionType.type" label="Purchase"/>
+        </div>
 
         <div class="flex gap-8 w-full">
             <TextInput required v-model="transactionData.invoice" class="w-full" placeholder="Transaction receipt invoice number" label="Transaction Invoice" />
             <DatePicker required v-model="transactionData.date" class="w-full" label="Transaction date" />
         </div>
 
-        <div class="flex items-center gap-8 w-full">
-            <TextInput v-model="transactionData.quantity" required class="w-full" placeholder="Quantity of items in transaction" label="Item Quantity" />
-            <SwitchInput class="px-3" label-location="top" v-model="transactionType.type" label="Purchase"/>
+<!--        <div class="flex items-center gap-8 w-full">-->
+<!--            <TextInput v-model="transactionData.quantity" required class="w-full" placeholder="Quantity of items in transaction" label="Item Quantity" />-->
+<!--        </div>-->
+
+        <!-- Store Transactions -->
+        <div class="gap-6 flex flex-col">
+
+            <div class="flex items-center justify-start w-full gap-2 px-2">
+                <p class="text-xs text-subtitle font-medium whitespace-nowrap">Store Transactions</p>
+                <div class="border-b border-border-light w-full py-1 mb-2 w-full m-auto"></div>
+            </div>
+
+            <template v-for="(store, index) in storeData" :key="index">
+
+                <div class="flex gap-8 w-full">
+                    <Dropdown
+                        class="!w-full h-16"
+                        :hide-label-on-mobile=false
+                        required
+                        v-model="store.id"
+                        :selected="store.name"
+                        :list="stores"
+                        :resource_list="{ display: 'name', value: 'id' }"
+                        placeholder="Choose Warehouse"
+                        label-direction="top"
+                        label="Warehouse"
+                    />
+                    <TextInput required v-model="store.quantity" class="w-full" placeholder="Transaction quantity" label="Transaction Quantity" />
+                </div>
+
+            </template>
+
         </div>
 
     </Form>
@@ -29,6 +61,7 @@
     import Form from "../../components/Form/Form.vue";
     import TextInput from "../../components/Form/TextInput.vue";
     import SwitchInput from "../../components/Form/SwitchInput.vue";
+    import Dropdown from "../../components/Dropdown.vue"
     import store from "../../store"
     import DatePicker from "../../components/Form/DatePicker.vue";
     import { useRoute } from "vue-router";
@@ -48,6 +81,7 @@
         quantity: '',
         invoice: ''
     })
+    const storeData = ref([])
 
     async function getTransaction(id) {
 
@@ -93,11 +127,36 @@
 
     }
 
+    const stores = ref([])
+    const loadingStore = ref(true)
+    async function getStores() {
+
+        return await axios.get('/api/stores')
+            .then(response => {
+                stores.value = response.data.data
+            })
+            .catch(error => {
+                alert('Error fetching Stores list ', error.data.message)
+            })
+            .finally(() => {
+                loadingStore.value = false
+            })
+    }
+
     onMounted(() => {
 
         getTransaction(useRoute().params.id).then(() => {
 
             const transaction = store.state.transaction.transaction.transaction
+            storeData.value = store.state.transaction.transaction.stores.map(store => {
+
+                return {
+                    id: store.id,
+                    name: store.store.name,
+                    quantity: store.quantity,
+                }
+
+            })
 
             transactionData.value.id = transaction.id
             transactionType.value.type = transaction.type === 'purchase'
@@ -112,6 +171,8 @@
             }
 
         })
+
+        getStores()
 
     })
 
