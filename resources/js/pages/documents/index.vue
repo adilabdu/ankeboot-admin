d90ed3a8b2e23b5c17cdf206caa672609e7df921
adilabdu <adilabdu68@gmail.com>
2023-03-11 07:44:46 +0300

    Fix Google Icon image size

--- resources/js/pages/documents/index.vue
+++ resources/js/pages/documents/index.vue
@@ -4,7 +4,7 @@
         <router-view />
 
         <button @click="loginWithGoogle" class="flex gap-2 items-center justify-center bg-white rounded-md border-[1.75px] border-border-dark font-medium px-4 py-2.5 w-fit text-black/90">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"  alt="Google Logo"/>
+            <img class="w-4" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"  alt="Google Logo"/>
             <span>Sign in with Google</span>
         </button>
 
